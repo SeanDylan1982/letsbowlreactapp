455cb2c7ccf64a12e0aac0213738a97a468961d9
SeanDylan1982 <74496368+SeanDylan1982@users.noreply.github.com>
2023-02-07 09:21:37 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "routes",
+    "database": "sample_training",
+    "dataSource": "letsbowl",
+    "projection": {
+        "_id": 1
+    }
+});
+            
+var config = {
+    method: 'post',
+    url: 'https://data.mongodb-api.com/app/data-nfjsy/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'fwebuouUsGqjIHrasy7KAl6jOoU0ofU8FKkXGnoo4uZZhCHjyaOEePeKtRpTsxNv',
+    },
+    data: data
+};
+            
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
